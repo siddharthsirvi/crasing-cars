d852e832219e0cb268308656b18b031df094c193
siddharthsirvi <74129899+siddharthsirvi@users.noreply.github.com>
2020-11-26 17:48:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# crasing-cars
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var car,wall 
+var speed,weight
+function setup() {
+  createCanvas(2000,2000);
+ car=createSprite(50,200,50,50);
+ wall=createSprite(1500,300,80,300);
+ speed=random(55,90);
+ weight=random(400,1500);
+ car.velocityX=speed;
+ wall.shapecolour=(80,80,80);
+
+}
+
+function draw() {
+  background(0);  
+ if(wall.x-car.x<(car.width+wall.width)/2){
+car .velocityX=0
+var deformation=0.5*weight*speed*speed/22509
+if(deformation>180){
+  car.shapecolour=color(255,0,0); 
+}
+if(deformation<180&&deformation>100){
+car.shapecolour=color(230,230,0)
+
+}
+if(deformation<100){
+car.shapecolour=color(0,255,0)
+
+}
+
+ } 
+  drawSprites();
+}
